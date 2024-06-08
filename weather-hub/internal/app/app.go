c34120e6c9b52b6cd4d79a4e1041095354eadbb3
Paweł Wójcik <avedonmedia@gmail.com>
2024-06-08 19:04:15 +0200

    add clickhouse client with base funcionalities

--- weather-hub/internal/app/app.go
+++ weather-hub/internal/app/app.go
@@ -2,19 +2,36 @@ package app
 
 import (
 	"log"
+	"sync"
 
+	chclient "github.com/wojcikp/go-weather-go/weather-hub/internal/ch_client"
 	rabbitmqclient "github.com/wojcikp/go-weather-go/weather-hub/internal/rabbitmq_client"
+	weatherfeedconsumer "github.com/wojcikp/go-weather-go/weather-hub/internal/weather_feed_consumer"
 )
 
 type App struct {
-	rabbitClient *rabbitmqclient.RabbitClient
+	rabbitClient     *rabbitmqclient.RabbitClient
+	clickhouseClient *chclient.ClickhouseClient
+	feedConsumer     *weatherfeedconsumer.Consumer
 }
 
-func NewApp(rabbitClient *rabbitmqclient.RabbitClient) *App {
-	return &App{rabbitClient}
+func NewApp(
+	rabbitClient *rabbitmqclient.RabbitClient,
+	clickhouseClient *chclient.ClickhouseClient,
+	feedConsumer *weatherfeedconsumer.Consumer,
+) *App {
+	return &App{rabbitClient, clickhouseClient, feedConsumer}
 }
 
 func (app App) Run() {
 	log.Print("weather hub app run")
-	app.rabbitClient.ReceiveMessages()
+	wg := &sync.WaitGroup{}
+	app.clickhouseClient.CreateWeatherTable()
+	wg.Add(1)
+	go app.rabbitClient.ReceiveMessages(wg)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go app.feedConsumer.Work(wg, app.clickhouseClient)
+	}
+	wg.Wait()
 }
---
+++ weather-hub/internal/city.go
@@ -0,0 +1,14 @@
+package internal
+
+import (
+	"time"
+)
+
+type CityWeatherData struct {
+	Name         string
+	Time         []time.Time
+	Temperatures []float64
+	WindSpeed    []float64
+	WeatherCodes []int
+	Error        error
+}
