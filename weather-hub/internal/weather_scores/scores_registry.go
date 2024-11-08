dcb7e663d004e397d5765517aedae95ba25dd372
Paweł Wójcik <avedonmedia@gmail.com>
2024-11-08 14:09:42 +0100

    fix scores id counter and add new score to registry

--- weather-hub/internal/weather_scores/scores_registry.go
+++ weather-hub/internal/weather_scores/scores_registry.go
@@ -2,13 +2,14 @@ package weatherscores
 
 import "log"
 
+var idCounter int
+var nextId = func() int {
+	idCounter++
+	return idCounter
+}
+
 func GetScoresList[T ScoreValue]() []IWeatherScore[T] {
 	var dummy T
-	var idCounter int
-	nextId := func() int {
-		idCounter++
-		return idCounter
-	}
 	switch v := any(dummy).(type) {
 	case string:
 		return []IWeatherScore[T]{
@@ -16,7 +17,9 @@ func GetScoresList[T ScoreValue]() []IWeatherScore[T] {
 			&MostRainyCity7d[T]{BaseWeatherScore{Id: nextId()}},
 		}
 	case float64:
-		return []IWeatherScore[T]{}
+		return []IWeatherScore[T]{
+			&AvgTemperatureWarsaw14d[T]{BaseWeatherScore{Id: nextId()}},
+		}
 	default:
 		log.Printf("ERROR: Unsupported score type: %v", v)
 		return []IWeatherScore[T]{}
